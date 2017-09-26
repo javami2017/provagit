27233e11003dad585c6c0bc0ed7a6847dce516cf
nomad <umadnomad@users.noreply.github.com>
2017-09-26 10:06:54 +0200

    Owner.java: aggiunta annotation @OnDelete sui cani del padrone: se un padrone viene cancellato, anche i suoi cani seguiranno la stessa sorte

--- hib_esempio_annotations/src/com/riccardofinazzi/hibernate/model/entity/Owner.java
+++ hib_esempio_annotations/src/com/riccardofinazzi/hibernate/model/entity/Owner.java
@@ -9,54 +9,62 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 @Entity
 public class Owner {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 
 	@Column
 	private String name;
 
-	/*
-	 * in db you will see no dog column in owner's table: an owner can have many
+	/* in db you will see no dog column in owner's table: an owner can have many
 	 * dogs, one dog can have one owner. Therefore, dogs will have one column to
-	 * store their owner id
-	 */
+	 * store their owner id */
 	@OneToMany(mappedBy = "owner")
+	@OnDelete(action = OnDeleteAction.CASCADE)
 	private Set<Dog> dogs;
 
-	public Owner() {
-	}
+	public Owner() {}
 
 	public Owner(int id, String name, Set<Dog> dogs) {
+
 		this.id = id;
 		this.name = name;
 		this.dogs = dogs;
 	}
 
 	public int getId() {
+
 		return id;
 	}
 
 	public void setId(int id) {
+
 		this.id = id;
 	}
 
 	public String getName() {
+
 		return name;
 	}
 
 	public void setName(String name) {
+
 		this.name = name;
 	}
 
 	public Set<Dog> getDogs() {
+
 		return dogs;
 	}
 
 	public void setDogs(Set<Dog> dogs) {
+
 		this.dogs = dogs;
 	}
 
