0ab8fe5421256f1f72076187ffea4f12c64eefe7
mattiamayta <31443791+mattiamayta@users.noreply.github.com>
2017-10-11 17:24:14 +0200

    aggiunti commenti a varie parti

--- ristorante/src/com/begear/ristorante/controller/GreMaRiMaServlet.java
+++ ristorante/src/com/begear/ristorante/controller/GreMaRiMaServlet.java
@@ -26,9 +26,9 @@ public class GreMaRiMaServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	private BusinessDelegate	bd		= new BusinessDelegate();
-	private Map<Integer, Dish>	dishes	= new HashMap<Integer, Dish>();
-	private Map<Integer, Table>	tables	= new HashMap<Integer, Table>();
+	private BusinessDelegate bd = new BusinessDelegate();
+	private Map<Integer, Dish> dishes = new HashMap<Integer, Dish>();
+	private Map<Integer, Table> tables = new HashMap<Integer, Table>();
 	{
 		bd.setServiceType(ServiceType.HIBERNATE);
 
@@ -40,8 +40,8 @@ public class GreMaRiMaServlet extends HttpServlet {
 			tables.put(table.getId(), table);
 		}
 	}
-	private Map<Integer, Client>	clienti;
-	private List<Order>				ordini;
+	private Map<Integer, Client> clienti;
+	private List<Order> ordini;
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -52,11 +52,12 @@ public class GreMaRiMaServlet extends HttpServlet {
 		sessionPhase = (String) req.getSession().getAttribute("sessionPhase");
 
 		switch (sessionPhase) {
-		case "1":
+		case "1": // selezione tavolo e inserimento clienti
 
 			requestPhase = req.getParameter("requestPhase");
 
 			if (requestPhase != null && requestPhase.equals("1")) {
+				// inserisce il numero del tavolo e il numero dei clienti
 
 				numTavolo = Integer.parseInt(req.getParameter("tavolo"));
 				numClienti = Integer.parseInt(req.getParameter("numero"));
@@ -66,16 +67,19 @@ public class GreMaRiMaServlet extends HttpServlet {
 
 			} else if (requestPhase != null && requestPhase.equals("2")) {
 
+				// prende il numero del tavolo e dei clienti
 				numTavolo = (Integer) req.getSession().getAttribute("tavoloSelezionato");
 				numClienti = (Integer) req.getSession().getAttribute("numeroClienti");
 
 				clienti = new HashMap<Integer, Client>();
 
+				// inserisce nella mappa i vari clienti sotto forma di oggetto Client
 				for (int i = 1; i <= numClienti; i++) {
 					Client c = bd.insertClient(req.getParameter("cliente_" + i), tables.get(numTavolo), 1);
 					clienti.put(c.getId(), c);
 				}
 
+				// proceedToOrder è true e quindi si può passare alla visualizzazione del menù
 				if (req.getParameter("proceedToOrder").equals("true")) {
 					req.getSession().setAttribute("menu", bd.getMenu());
 					req.getSession().setAttribute("sessionPhase", "2");
@@ -83,7 +87,7 @@ public class GreMaRiMaServlet extends HttpServlet {
 				req.getSession().setAttribute("clienti", clienti);
 			}
 			break;
-		case "2":
+		case "2": // visualizzazione del menù
 
 			req.getSession().setAttribute("menu", bd.getMenu());
 			ordini = new ArrayList<Order>();
@@ -91,11 +95,11 @@ public class GreMaRiMaServlet extends HttpServlet {
 			String idCliente = req.getParameter("clientId");
 			String idPiatto = null;
 
+			// scorre i vari piatti del menù per inserire quelli selezionati nell'ordine
 			for (int i = 1; i <= dishes.size(); i++) {
 				if ((idPiatto = req.getParameter("cb_" + i)) != null) {
-
 					Order o = new Order(dishes.get(Integer.parseInt(idPiatto)),
-										clienti.get(Integer.parseInt(idCliente)), 0, 0, new Date());
+							clienti.get(Integer.parseInt(idCliente)), 0, 0, new Date());
 					ordini.add(o);
 					bd.insertOrder(o);
 				}
@@ -103,6 +107,7 @@ public class GreMaRiMaServlet extends HttpServlet {
 
 			req.getSession().setAttribute("clienteSelezionato", idCliente);
 			req.getSession().setAttribute("sessionPhase", "3");
+			//adesso la sessionPhase="3" -> fine parte inserimento ordini
 			break;
 		}
 
